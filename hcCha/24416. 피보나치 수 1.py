b1f900878cd1df7a67515e053e0e29c2d7596508
Bright <ckguscjf03@gmail.com>
2023-08-10 23:08:38 +0900

    230810 updata

---
+++ hcCha/24416. 피보나치 수 1.py
@@ -0,0 +1,16 @@
+# 피보나치 수열 동적 계획법 / DP / 다이나믹 프로그래밍
+def fibonacci(n):
+    global cnt2
+    f = [0, 1, 1]
+    for i in range(3, n + 1):
+        cnt2 += 1
+        f.append(f[i - 1] + f[i - 2])
+    return f[n]
+
+
+n = int(input())
+cnt2 = 0
+
+# 재귀함수 실행 횟수는 피보나치 수만큼 늘어나기에 DP 리턴값을 받고
+# 그 실행횟수를 출력한다.
+print(fibonacci(n), cnt2)
