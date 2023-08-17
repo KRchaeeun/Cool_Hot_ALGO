4398682f2811a3737b56b517e849c97c3b653e24
TaeWan Kim <TaeWan>
2023-08-17 22:47:58 +0900

    식닥 입구 대기 줄

--- twKim/암호생성기.py
+++ twKim/암호생성기.py
@@ -5,6 +5,7 @@ for tc in range (1, 11) :
     while N_list[-1] > 0 :                      # N_list의 끝이 0 이하이면 종료하는 while 문
         for i in range (1,6) :                  # 5번이 한 싸이클로 굴려줌
             N_list.append(N_list.pop(0)-i)      # 싸이클에 맞는 수식을 넣어줌
+            
             if N_list[-1] <= 0 :                # 한번 할때마다 N_list의 뒤를 검사해
                 break                           # 0 이하이면 for문 탈출
     
