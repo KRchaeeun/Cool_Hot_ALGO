02ee185fb38e30f6f7397f386456c95bc374c819
GY KWON <yeol3011@gmail.com>
2023-09-09 21:10:21 +0900

    0909

--- gyKwon/g4_3190_뱀.py
+++ gyKwon/g4_3190_뱀.py
@@ -1,18 +1,19 @@
+**************미완성
+
 def bfs(i,j):
+    global di,dj
     q = []
     q.append((i,j))
     visited = [[0]*N for _ in range(N)]
-    cnt = 0
-    snake = []
-    while q:
+
+    while True:
         i,j = q.pop(0)
-        snake.append((i,j))
-        ni,nj = i,j+1
-        for a in turn:
-            if cnt >= a[0]:
-                if a[1] = 'L':
-                    ni,nj = i+1,j
-                else:
+        mv = 1
+        ni = i+di[mv%4]
+        nj = j+dj[mv%4]
+        if ni>N and ni<0 and nj>N and nj<0 and (ni,nj) in q:
+            break
+        for
 
 
 N = int(input())
@@ -21,10 +22,10 @@ arr = [[0]*N for _ in range(N)]
 turn = []
 for _ in range(K):
     i,j = map(int,input().split())
-    arr[i][j] = 1
-    arr[j][i] = 1
+    arr[i-1][j-1] = 1
 L = int(input())
 for _ in range(L):
     a,b = input().split()
     turn.append([int(a),b])
-print(turn)
+di = [-1,0,1,0]
+dj = [0,1,0,-1]
--- gyKwon/s2_24479_깊이우선탐색1.py
+++ gyKwon/s2_24479_깊이우선탐색1.py
@@ -1,38 +1,39 @@
-########메모리 초과!!!!!##############
-
-def dfs(N,rdmap,I):
+import sys
+input = sys.stdin.readline
+# 재귀함수를 사용하기 위해 재 설정
+sys.setrecursionlimit(1000000)
+# 깊이 우선 탐색 함수 설정
+def dfs(R):
+    global cnt
+    global visited
     stack = []
-    visited = [0]*(N+1)
-    visited[I] = 1
-    print(I)
-    while True:
+    stack.append(R)
+    # 1 번 노드부터 탐색하기 때문에 cnt 초기값을 1로 설정
+    visited[R] = cnt
+    cnt += 1
+    # 재귀함수-> 연결리스트는 오름차순으로 정렬되어있음
+    # 오름차순으로 탐색하면서 방문하지 않았다면 해당 노드로 이동
+    # 방문할 수 있는 노드가 존재할때까지(간선이 연결되어 있는 경우) 탐색
+    # 간선이 연결되어 있지 않아 방문 할 수 없을 시 0 출력
+    # 이 경우에는 노드가 5까지 있지만, 5로 연결된 간선이 없어서 0 출력
+    for w in arr[R]:
+        if visited[w] == 0:
+            dfs(w)
 
-        for w in range(1,N+1):
-            if rdmap[I][w] == 1 and visited[w] == 0:
-                stack.append(I)
-                I = w
-                print(I)
-                visited[I] = 1
-                break
-        else:
-            if stack:
-                I = stack.pop()
-            else:
-                print(0)
-                break
-    return
+N,M,R = map(int,input().split())
+arr = [[] for _ in range(N+1)]
+visited = [0]*(N+1)
+cnt = 1
+# 메모리 초과를 고려하여 연결리스트로 받아주기
+for _ in range(M):
+    i,j = map(int,input().split())
+    arr[i].append(j)
+    arr[j].append(i)
+# 오름차순으로 탐색하기 때문에 정렬
+for k in range(N+1):
+    arr[k].sort()
 
-
-N,M,I = list(map(int,input().split()))
-road = []
-for _ in range(N):
-    s,g = list(map(int,input().split()))
-    road.append(s)
-    road.append(g)
-rdmap = [[0]*(N+1) for _ in range(N+1)]
-for i in range(M):
-    v1,v2 = road[i*2], road[i*2+1]
-    rdmap[v1][v2] = 1
-    rdmap[v2][v1] = 1
-
-dfs(N,rdmap,I)
+dfs(R)
+# for a in range(1,N+1):
+#     print(visited[a])
+print(arr)
