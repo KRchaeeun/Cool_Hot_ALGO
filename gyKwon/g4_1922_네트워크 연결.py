ce553db23725fd67d8e5edd06764c8976a98edef
GY KWON <yeol3011@gmail.com>
2023-09-21 22:51:36 +0900

    0921_2

---
+++ gyKwon/g4_1922_네트워크 연결.py
@@ -0,0 +1,78 @@
+## prim
+import sys,heapq
+input = sys.stdin.readline
+
+def prim(start):
+    heap = []
+    visited = [0]*(V+1)
+    heapq.heappush(heap,(0,start))
+    suma = 0
+    while heap:
+        w,v = heapq.heappop(heap)
+        if visited[v]:
+            continue
+        visited[v] = 1
+        suma += w
+        for nv,nw in graph[v]:
+            if visited[nv]:
+                pass
+            heapq.heappush(heap,(nw,nv))
+    return suma
+
+V = int(input())
+E = int(input())
+graph = [[] for _ in range(V+1)]
+for _ in range(E):
+    a, b ,c = map(int,input().split())
+    graph[a].append((b,c))
+    graph[b].append((a,c))
+
+print(prim(1))
+
+##kruskal
+# 대표차 찾기
+def find_set(x):
+    if parents[x] == x:
+        return x
+    parents[x] = find_set(parents[x])
+    return parents[x]
+
+# 대표자 저장 과정 (루트의 동일 여부로 판단)
+def union(x,y):
+    x = find_set(x)
+    y = find_set(y)
+
+    if x == y:
+        return
+    if x<y:
+        parents[y] = x
+    else:
+        parents[x] = y
+
+V = int(input())
+E = int(input())
+edge = []
+for _ in range(E):
+    a,b,c = map(int,input().split())
+    edge.append([a,b,c])
+# 가중치를 기준으로 오름차순정렬
+edge.sort(key=lambda x:x[2])
+
+# make set 과정
+parents = [i for i in range(V+1)]
+
+# 정점 방문 횟수 기록
+cnt = 0
+# 지나온 간선의 가중치 기록
+suma = 0
+# 찾을 수 있는 모든 최소 신장 트리의 경우의 수 찾기
+for a,b,c, in edge:
+    # 싸이클이 발생하지 않을때마다 실행
+    if find_set(a) != find_set(b):
+        cnt += 1
+        suma += c
+        union(a,b)
+        # 최소 신장 트리를 구하면
+        if cnt == V:
+            break
+print(suma)
