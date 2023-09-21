ae2f37b783beefe382c1ccfd3da3b7ea8a5915f5
GY KWON <yeol3011@gmail.com>
2023-09-21 22:54:26 +0900

    0921__

--- gyKwon/g4_1922_네트워크 연결.py
+++ gyKwon/g4_1922_네트워크 연결.py
@@ -13,7 +13,7 @@ def prim(start):
             continue
         visited[v] = 1
         suma += w
-        for nv,nw in graph[v]:
+        for nw,nv in graph[v]:
             if visited[nv]:
                 pass
             heapq.heappush(heap,(nw,nv))
@@ -24,8 +24,8 @@ E = int(input())
 graph = [[] for _ in range(V+1)]
 for _ in range(E):
     a, b ,c = map(int,input().split())
-    graph[a].append((b,c))
-    graph[b].append((a,c))
+    graph[a].append((c,b))
+    graph[b].append((c,a))
 
 print(prim(1))
 
